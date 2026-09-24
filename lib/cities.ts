f95611eab84cdb6bc1faebb9597a4b5b6cf46b21
Sharpie <77106468+sharpiexoxo@users.noreply.github.com>
2026-09-24 12:38:38 +0200

    Add additional cities to the CITIES constant

--- lib/cities.ts
+++ lib/cities.ts
@@ -8,6 +8,21 @@ export const CITIES = [
   { value: "odense",     label: "Odense"    },
   { value: "aalborg",    label: "Aalborg"   },
   { value: "randers",    label: "Randers"   },
+  { value: "esbjerg",     label: "Esbjerg"    },
+  { value: "horsens",     label: "Horsens"    },
+  { value: "vejle",       label: "Vejle"      },
+  { value: "roskilde",    label: "Roskilde"   },
+  { value: "herning",     label: "Herning"    },
+  { value: "silkeborg",   label: "Silkeborg"  },
+  { value: "naestved",    label: "Næstved"    },
+  { value: "fredericia",  label: "Fredericia" },
+  { value: "viborg",      label: "Viborg"     },
+  { value: "kolding",     label: "Kolding"    },
+  { value: "holstebro",   label: "Holstebro"  },
+  { value: "slagelse",    label: "Slagelse"   },
+  { value: "hilleroed",   label: "Hillerød"   },
+  { value: "soenderborg", label: "Sønderborg" },
+  { value: "hjorring",    label: "Hjørring"   },
 ] as const;
 
 export type CityValue = (typeof CITIES)[number]["value"];
